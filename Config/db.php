00a7329873ef95aad5b1d12e12e3cf3f7dacb47b
Amand <amand.witters@coding-academy.fr>
2017-04-04 09:35:46 +0200

    setup

---
+++ Config/db.php
@@ -0,0 +1,34 @@
+<?php
+
+class dbConn{
+
+    const DEFAULT_SQL_USER = 'root';
+    const DEFAULT_SQL_HOST = 'localhost';
+    const DEFAULT_SQL_PASS = '';
+    const DEFAULT_SQL_DTB = 'tasks';
+
+    protected static $db;
+
+    private function __construct() {
+
+        try {
+            self::$db = new PDO('mysql:dbname='.self::DEFAULT_SQL_DTB.';host='.self::DEFAULT_SQL_HOST,self::DEFAULT_SQL_USER ,self::DEFAULT_SQL_PASS);
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+
+    }
+
+    public static function getConnection() {
+
+        if (!self::$db) {
+            new dbConn();
+        }
+        return self::$db;
+    }
+
+}
+
+?>
---
+++ Controllers/ArticlesController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amand
+ * Date: 04/04/2017
+ * Time: 09:30
+ */
