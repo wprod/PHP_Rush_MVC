4baeaf096d23f9ca9b43d5748162a4c3ebedffd6
wprod <witters.amand@gmail.com>
2017-04-05 09:35:31 +0200

    mercredi

--- Config/core.php
+++ Config/core.php
@@ -5,3 +5,31 @@
  * Date: 04/04/2017
  * Time: 09:33
  */
+
+function autoload_class_multiple_directory($class_name)
+{
+
+    # List all the class directories in the array.
+    $array_paths = array(
+        '../Controllers/',
+        '../Models/',
+        '../router/'
+    );
+
+    # Count the total item in the array.
+    $total_paths = count($array_paths);
+
+    # Set the class file name.
+    $file_name = $class_name.'.php';
+
+    # Loop the array.
+    for ($i = 0; $i < $total_paths; $i++)
+    {
+        if(file_exists($array_paths[$i].$file_name))
+        {
+            include_once $array_paths[$i].$file_name;
+        }
+    }
+}
+
+spl_autoload_register('autoload_class_multiple_directory');
--- Webroot/index.php
+++ Webroot/index.php
@@ -1,12 +1,9 @@
 <?php
 
-include_once "../Controllers/ArticlesController.php";
-include_once "../Controllers/UsersController.php";
+include_once "../Config/core.php";
 
-$test1 = new ArticleController();
 $test2 = new UsersController();
 
-var_dump($test1->get_article(1));
 $test2->add_user("YoMAMA","blabla","yomama@gmail.com","Admin");
 $test2->update_user("1","YooooMAMA","blabla","yoooomama@gmail.com","Admin");
 $test2->delete_user(2);
