af087aa1d1041cbcef64e81353200ac1947c7f60
wprod <witters.amand@gmail.com>
2017-04-05 10:24:12 +0200

    htacces

---
+++ Config/configuration.php
@@ -0,0 +1,8 @@
+<?php
+abstract class Configuration {
+    const DEFAULT_SQL_USER = 'root';
+    const DEFAULT_SQL_HOST = 'localhost';
+    const DEFAULT_SQL_PASS = '';
+    const DEFAULT_SQL_DTB = 'blog_mvc';
+}
+
