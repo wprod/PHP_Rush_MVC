8a0fcc358d3e0212fec734aeba7e549f0021004d
wprod <witters.amand@gmail.com>
2017-04-06 14:28:15 +0200

    twig nav index

--- Config/core.php
+++ Config/core.php
@@ -8,20 +8,16 @@
 
 function autoload_class_multiple_directory($class_name)
 {
-
     # List all the class directories in the array.
     $array_paths = array(
         '../Controllers/',
         '../Models/',
         '../Src/'
     );
-
     # Count the total item in the array.
     $total_paths = count($array_paths);
-
     # Set the class file name.
     $file_name = $class_name.'.php';
-
     # Loop the array.
     for ($i = 0; $i < $total_paths; $i++)
     {
@@ -31,6 +27,15 @@ function autoload_class_multiple_directory($class_name)
         }
     }
 }
-
 spl_autoload_register('autoload_class_multiple_directory');
+
+
+//DISPATCHER TODO
+$router = new Router($_GET['url']);
+$router->get('/', function () {
+    echo "Todo homepage";
+});
+$router->get('/users', "Users#get_users");
+$router->get('/user_:id', "Users#get_user")->with('id', '[0-9]+');
+$router->run();
 
--- Webroot/index.php
+++ Webroot/index.php
@@ -2,30 +2,5 @@
 
 include_once "../Config/core.php";
 
-//$test2 = new UsersController();
-//
-//$test2->add_user("Brenda","blabla","brenda@gmail.com","Admin");
-//$test2->update_user("1","YooooMAMA","blabla","yoooomama@gmail.com","Admin");
-//$test2->delete_user(2);
-//
-//var_dump($test2->get_users());
-
-//$router = new Router($_GET['url']);
-//$router->get('/', function(){ echo "Homepage"; });
-//$router->get('/posts', function(){ echo 'Tous les articles'; });
-//$router->get('/article/:slug-:id/:page', "Posts#show")->with('id', '[0-9]+')->with('page', '[0-9]+')->with('slug', '([a-z\-0-9]+)');
-//$router->get('/article/:slug-:id', "Posts#show")->with('id', '[0-9]+')->with('slug', '([a-z\-0-9]+)');
-//
-//$router->run();
-
-
-$router = new Router($_GET['url']);
-
-$router->get('/', function () {
-    echo "Todo homepage";
-});
-$router->get('/users', "Users#get_users");
-$router->get('/user-:id', "Users#get_user")->with('id', '[0-9]+');
-$router->run();
 
 ?>
